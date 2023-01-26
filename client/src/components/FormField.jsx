a3dcfc9eb8e7867898712cf891382c3f4de94e94
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-01-26 10:42:22 -0500

    Add FormField component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
-const FormField = () => {
+// We need to get access to all theprompt that we passed into our form field component
+// We can do that by destructuring everything from props (parameters)
+const FormField = ({ LabelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
-    <div>FormField</div>
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+      <label 
+        htmlFor={name}
+        className='block text-sm font-medium text-gray-900'>
+        
+        {LabelName}
+      
+      </label>
+      {isSurpriseMe && (
+        <button
+          type='button'
+          onClick={handleSurpriseMe}
+          className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+          >
+          Surprise me
+
+        </button>
+      )}
+      </div>
+      <input 
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-1g focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-fill p-3'
+      />
+    </div>
   )
 }
 
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -7,8 +7,63 @@ import { FormField, Loader } from '../components';
 
 
 const CreatePost = () => {
+  // This will allow us to navigate back to the home page once the post is created
+  const navigate = useNavigate();
+  
+  const [form, setForm] = useState({
+    name: '',
+    prompt: '',
+    photo: '',
+  })
+  // Using this while waiting to get back image from API
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = (e) => {
+
+  }
+
+  const handleSurpriseMe = () => {
+
+  }
   return (
-    <div>CreatePost</div>
+    <section className='max-w-7x1 mx-auto'>
+      <div>
+        <h1 className='font-extrabold text-[#222328] text-[32px]'>
+          Create
+        </h1>
+        <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'>
+        Create imaginative and visually stunning images through DALL-E AI and share them with the community
+        </p>
+      </div>
+
+      <form className='mt-16 max-w-3x1' onSubmit={handleSubmit}>
+        <div className='flex flex-col gap-5'>
+          <FormField 
+            LabelName="Your name"
+            type='text'
+            name='name'
+            placeholder='John Doe'
+            value={form.name}
+            handleChange={handleChange}
+          />
+           <FormField 
+            LabelName="Prompt"
+            type='text'
+            name='prompt'
+            placeholder='A plush toy robot sitting against a yellow wall'
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
   )
 }
 
