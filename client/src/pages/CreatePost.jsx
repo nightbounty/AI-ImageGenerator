6a2db1c0e27711219bb5b0daf61935064cee0867
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-01-26 12:45:07 -0500

    Add CreatePost page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,19 +19,29 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const generateImage = () => {
+
+  }
+
   const handleSubmit = () => {
 
   }
 
+  // take the event, the key press event and its going to call the set formstate 
+  // there we want to spread the entire form and we want to update e.target.name with the newly created e.target.value
   const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
 
   }
 
+  // We get a randomPrompt then we will call the setForm state
+  // to spread out the form to update the prompt to the randomPropt
   const handleSurpriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
   }
   return (
-    <section className='max-w-7x1 mx-auto'>
+    <section className='max-w-7xl mx-auto'>
       <div>
         <h1 className='font-extrabold text-[#222328] text-[32px]'>
           Create
@@ -41,7 +51,7 @@ const CreatePost = () => {
         </p>
       </div>
 
-      <form className='mt-16 max-w-3x1' onSubmit={handleSubmit}>
+      <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
         <div className='flex flex-col gap-5'>
           <FormField 
             LabelName="Your name"
@@ -61,7 +71,59 @@ const CreatePost = () => {
             isSurpriseMe
             handleSurpriseMe={handleSurpriseMe}
           />
+
+          {/* Creating a place where an Ai generated image will be shown 
+              and also show a preview of the image in case it hasn't been already generated
+          */}
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {/* if there is a form.photo then we can show a self-closing tag
+            that's going to have a source form.photo */}
+            { form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+              
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {/* Show the loader*/}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
         </div>
+        
+        {/* Making our Generate button and wrapping it*/}
+        <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+        
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+          {/* Making Share button*/}
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
+          </button>
+        </div>
+
       </form>
     </section>
   )
