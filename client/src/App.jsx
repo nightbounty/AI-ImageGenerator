b03aec727c990fe052cd3e60ffe36e9b0dbfd572
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-01-25 22:01:41 -0500

    Add Header and Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter,Link, Route, Routes } from 'react-router-dom';
+
+
+import { AILogo } from './assets';
+import { Home, CreatePost } from './pages';
 
 const App = () => {
   return (
-    <h1 className='text-3x1 font-bold underline'>
-      HelloWord!
-    </h1>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ef4]'>
+        <Link to="/">
+          <img src={AILogo} alt="logo" className='h-12 object-contain'/>
+        </Link>
+        <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md' >
+          Create 
+        </Link>
+      </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[cal(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/create-post' element={<CreatePost/>}/>
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import AILogo from './AILogo.png';
 
 export {
   download,
   logo,
+  AILogo,
   preview,
 };
