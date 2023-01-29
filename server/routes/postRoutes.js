4a4b3a531e2f22d5eb03666725bee246f3c75ba7
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-01-29 16:52:42 -0500

    Add Cloudinary configuration

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,12 @@ dotenv.config();
 
 const router = express.Router();
 
+// Configuration of cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
 export default router;
